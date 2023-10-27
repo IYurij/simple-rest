03986b927f17ddcc088f395fa5d887f2abd420fe
IYurij <120470454+IYurij@users.noreply.github.com>
2023-10-27 12:41:09 +0700

    Added POJO Message class

---
+++ src/main/java/yurij/study/entity/Message.java
@@ -0,0 +1,29 @@
+package yurij.study.entity;
+
+/**
+ * POJO Message class.
+ */
+public class Message {
+    private String id;
+    private String text;
+
+    public Message() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    private void setText(String text) {
+        this.text = text;
+    }
+}
